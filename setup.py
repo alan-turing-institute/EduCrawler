63be8639dcc5eaeed87994d568533176c27d1f30
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2020-10-29 09:55:32 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.8',  # Required
+    version='0.5.9',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -151,6 +151,7 @@ setup(
         'webdriver-manager>=3.2.2',
         'pandas>=0.25.3',
         'tabulate>=0.8.7',
+        'pyyaml>=5.1.1',
     ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
